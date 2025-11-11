761d696574e3edfacc7fccb9b70f696c05eb841a
Antônio Frota <frota.monteiro@aluno.uece.br>
2025-11-11 17:47:31 -0300

    terminei a questao do codewars

--- Questões/Code Wars/SquareEveryDigit.c
+++ Questões/Code Wars/SquareEveryDigit.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    int number; 
-    char vetor[100];
-    int aux, aux2, contador;
-    printf("type the number: ");
+int main(int number){ 
     scanf("%d", &number);
+    int aux, contador = 0;
+    if(number != 0){
+        aux = number;
+        while(aux != 0){
+        aux = aux/10;
+        contador++; // contador é o numero de digitos
+        }
+        int digitos[contador+1];
+        for(int i = contador-1; i >= 0 ; i--){
+            aux = number%10;
+            digitos[i] = aux;
+            number /= 10;
+        }
 
-    sprintf(vetor, "%d", number);
-    printf("%s", vetor);
-
+        for(int i=0; i < contador; i++){
+            digitos[i] = digitos[i] * digitos[i];
+        }
+        for(int i=1; i < contador; i++){
+            if(digitos[i] >= 10){
+                digitos[0] = (digitos[0] * 100) + digitos[i];
+            }else if(digitos[i] < 10){
+                digitos[0] = (digitos[0] * 10) + digitos[i];
+            }
+        }
+        number = digitos[0];
+    }
+    return number;
 }
 
